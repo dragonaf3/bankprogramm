3c3ccf071a335aa95fa53584fdb2ce7ab293d45f
drago <97@dragonaf.eu>
2023-11-05 20:57:06 +0100

    Javadocs erstellt

--- src/main/java/bankprojekt/verwaltung/Bank.java
+++ src/main/java/bankprojekt/verwaltung/Bank.java
@@ -4,6 +4,9 @@ import bankprojekt.verarbeitung.*;
 
 import java.util.*;
 
+/**
+ * Diese Klasse repräsentiert eine Bank mit Kontenverwaltung
+ */
 public class Bank {
 
     private final long bankleitzahl;
@@ -12,20 +15,37 @@ public class Bank {
     private static final double STANDARD_DISPO = 1000;
 
 
+    /**
+     * Erzeugt eine neue Bank mit der angegebenen Bankleitzahl.
+     * @param bankleitzahl die Bankleitzahl der Bank
+     */
     public Bank(long bankleitzahl) {
         this.bankleitzahl = bankleitzahl;
         this.kontenListe = new HashMap<>();
         this.vergebeneKontonummern = new HashSet<>();
     }
 
+    /**
+     * Liefert die Bankleitzahl der Bank.
+     * @return die Bankleitzahl der Bank
+     */
     public long getBankleitzahl() {
         return bankleitzahl;
     }
 
+    /**
+     * Liefert die Liste aller Konten der Bank.
+     * @return die Liste aller Konten der Bank
+     */
     public Map<Long, Konto> getKontenListe() {
         return kontenListe;
     }
 
+    /**
+     * Erstellt ein neues Girokonto für den angegebenen Kunden.
+     * @param inhaber der Inhaber des Kontos
+     * @return die neu vergebene Kontonummer des Girokontos zurück
+     */
     public long girokontoErstellen(Kunde inhaber) {
         long neueKontennummer = erstelleNeueKontonummer();
         Girokonto girokonto = new Girokonto(inhaber, neueKontennummer, STANDARD_DISPO);
@@ -33,6 +53,11 @@ public class Bank {
         return neueKontennummer;
     }
 
+    /**
+     * Erstellt ein neues Sparbuch für den angegebenen Kunden.
+     * @param inhaber der Inhaber des Kontos
+     * @return die Kontonummer des neuen Sparbuchs
+     */
     public long sparbuchErstellen(Kunde inhaber) {
         long neueKontonummer = erstelleNeueKontonummer();
         Sparbuch sparbuch = new Sparbuch(inhaber, neueKontonummer);
@@ -40,6 +65,11 @@ public class Bank {
         return neueKontonummer;
     }
 
+
+    /**
+     * Erstellt einen String von allen Kontonummern und den zugehörigen Kontoständen.
+     * @return alle Kontonummern und Kontostände der Bank als String
+     */
     public String getAlleKonten() {
         StringBuilder alleKonten = new StringBuilder();
         for (Konto konto : kontenListe.values()) {
@@ -48,20 +78,45 @@ public class Bank {
         return String.valueOf(alleKonten);
     }
 
+    /**
+     * Erstellt eine Liste aller gültigen Kontonummern der Bank.
+     * @return die Liste aller gültigen Kontonummern der Bank.
+     */
     public List<Long> getAlleKontonummern() {
         return new ArrayList<>(kontenListe.keySet());
     }
 
+    /**
+     * Hebt den gewünschten Betrag vom angegebenen Konto ab.
+     * @param von   Kontonummer des Kontos von welchem das Geld abgehoben werden soll
+     * @param betrag Betrag welcher abgehoben werden soll
+     * @return true, wenn die Abhebung geklappt hat, sonst false
+     * @throws UngueltigeKontonummerException wenn die Kontonummer ungültig ist
+     * @throws GesperrtException wenn das Konto gesperrt ist
+     * @throws IllegalArgumentException wenn der Betrag negativ oder unendlich oder NaN ist
+     */
     public boolean geldAbheben(long von, double betrag) throws UngueltigeKontonummerException, GesperrtException, IllegalArgumentException {
         pruefeObKontonummerUngueltig(von);
         return kontenListe.get(von).abheben(betrag);
     }
 
-    public void geldEinzahlen(long von, double betrag) throws UngueltigeKontonummerException, IllegalArgumentException {
-        pruefeObKontonummerUngueltig(von);
-        kontenListe.get(von).einzahlen(betrag);
+    /**
+     * Zahlt den gewünschten Betrag auf das angegebene Konto ein.
+     * @param auf   Kontonummer des Kontos auf welches das Geld eingezahlt werden soll
+     * @param betrag Betrag welcher eingezahlt werden soll
+     * @throws UngueltigeKontonummerException wenn die Kontonummer ungültig ist
+     * @throws IllegalArgumentException wenn der Betrag negativ oder unendlich oder NaN ist
+     */
+    public void geldEinzahlen(long auf, double betrag) throws UngueltigeKontonummerException, IllegalArgumentException {
+        pruefeObKontonummerUngueltig(auf);
+        kontenListe.get(auf).einzahlen(betrag);
     }
 
+    /**
+     * Löscht das Konto mit der angegebenen Kontonummer.
+     * @param nummer Kontonummer des Kontos, welches gelöscht werden soll
+     * @return true, wenn das Konto erfolgreich gelöscht wurde, sonst false
+     */
     public boolean kontoLoeschen(long nummer) {
         if (kontenListe.containsKey(nummer)) {
             kontenListe.remove(nummer);
@@ -71,11 +126,28 @@ public class Bank {
         return false;
     }
 
+    /**
+     * Liefert den Kontostand des Kontos mit der angegebenen Kontonummer zurück.
+     * @param nummer Kontonummer des Kontos dessen Kontostand zurückgegeben werden soll
+     * @return Kontostand des Kontos
+     * @throws UngueltigeKontonummerException wenn die Kontonummer ungültig ist
+     */
     public double getKontostand(long nummer) throws UngueltigeKontonummerException {
         pruefeObKontonummerUngueltig(nummer);
         return kontenListe.get(nummer).getKontostand();
     }
 
+    /**
+     * Überweist einen gewünschten Betrag von einem überweisungsfähigen Konto zu einem anderen überweisungsfähigen Konto.
+     * @param vonKontonr Kontonummer des Kontos, von welchem die Überweisung getätigt werden soll
+     * @param nachKontonr Kontonummer des Kontos, welches die Überweisung erhalten soll
+     * @param betrag Überweisungsbetrag
+     * @param verwendungszweck Verwendungszweck
+     * @return true, wenn die Überweisung durchgeführt wurde, sonst false
+     * @throws UngueltigeKontonummerException wenn die Kontonummer ungültig ist
+     * @throws NichtUeberweisungsfaehigException wenn das Konto nicht überweisungsfähig ist
+     * @throws GesperrtException wenn das Konto gesperrt ist
+     */
     public boolean geldUeberweisen(long vonKontonr, long nachKontonr, double betrag, String verwendungszweck) throws UngueltigeKontonummerException, NichtUeberweisungsfaehigException, GesperrtException {
         pruefeObKontonummerUngueltig(vonKontonr);
         pruefeObKontonummerUngueltig(nachKontonr);
