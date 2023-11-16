99dc5fdd2c46ed46e2b6d86dccdd2a274f62b88d
drago <97@dragonaf.eu>
2023-11-16 01:06:38 +0100

    geldUeberweisen Methode

--- src/test/java/bankprojekt/verwaltung/BankTest.java
+++ src/test/java/bankprojekt/verwaltung/BankTest.java
@@ -3,7 +3,7 @@ package bankprojekt.verwaltung;
 import bankprojekt.verarbeitung.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -11,82 +11,77 @@ import static org.mockito.Mockito.*;
 class BankTest {
 
     private Bank bank;
-    @Mock
-    private Kunde kunde1;
-    @Mock
-    private Konto kontoUeberweisungsfähig;
-    @Mock
-    private Kunde kunde2;
-    @Mock
-    private Konto kontoUeberweisungsfähig2;
-    @Mock
-    private Kunde kunde3;
-    @Mock
-    private Konto kontoNichtUeberweisungsfähig;
+    private Konto vonKontoMock, nachKontoMock;
+    private Kunde dummy;
 
     @BeforeEach
     void setUp() {
-        bank = new Bank(12345); // Bankleitzahl als Beispielwert
+        bank = new Bank(12345678); // Erstellt eine Bank mit einer fiktiven Bankleitzahl
 
-        // Erstellen von Mocks für Konto-Objekte
-        kontoUeberweisungsfähig = mock(Girokonto.class);
-        kontoUeberweisungsfähig2 = mock(Girokonto.class);
-        kontoNichtUeberweisungsfähig = mock(Konto.class);
+        // Erstellen von Mock-Objekten für die Konten
+        vonKontoMock = mock(Girokonto.class);
+        nachKontoMock = mock(Girokonto.class);
 
-        // Erstellen von Mocks für Kunden-Objekte
-        kunde1 = mock(Kunde.class);
-        kunde2 = mock(Kunde.class);
-        kunde3 = mock(Kunde.class);
+        // Erstellen eines Dummy-Kunden
+        dummy = mock(Kunde.class);
 
-        when(kunde1.getName()).thenReturn("Empfaenger");
-        when(kunde2.getName()).thenReturn("Sender");
-
-        // Fügen Sie Mock-Konten in die Bank ein (dies erfordert möglicherweise eine Änderung in der Bankklasse, um dies zu ermöglichen)
-        bank.mockEinfuegen(kontoUeberweisungsfähig);
-        bank.mockEinfuegen(kontoUeberweisungsfähig2);
-        bank.mockEinfuegen(kontoNichtUeberweisungsfähig);
-
-        when(kontoUeberweisungsfähig.getKontonummer()).thenReturn(1L);
-        when(kontoUeberweisungsfähig2.getKontonummer()).thenReturn(2L);
-        when(kontoNichtUeberweisungsfähig.getInhaber()).thenReturn(mock(kunde1));
-        when(kontoUeberweisungsfähig.getInhaber()).thenReturn(mock(kunde2));
+        when(dummy.getName()).thenReturn("Max Muster");
 
 
+        // Definieren des Verhaltens für getInhaber() der Mock-Objekte
+        when(vonKontoMock.getInhaber()).thenReturn(dummy);
+        when(nachKontoMock.getInhaber()).thenReturn(dummy);
     }
 
     @Test
-    void geldUeberweisen_normal_true_Test() throws Exception {
-        when(((Ueberweisungsfaehig)kontoUeberweisungsfähig).ueberweisungAbsenden(500.0,"Empfaenger",2,12345,"Überweisungstest")).thenReturn(true);
-        doNothing().when((Ueberweisungsfaehig)kontoUeberweisungsfähig2).ueberweisungEmpfangen(500.0,"Sender",1,12345,"Überweisungstest");
+    void testGeldUeberweisenErfolgreich() throws UngueltigeKontonummerException, NichtUeberweisungsfaehigException, GesperrtException {
+        // Vorbereiten der Testdaten
+        long vonKontonr = 1, nachKontonr = 2;
+        double betrag = 100.0;
+        String verwendungszweck = "Testüberweisung";
 
-        boolean result = bank.geldUeberweisen(1, 2, 500.0, "Überweisungstest");
+        // Hinzufügen der Mock-Konten zur Bank
+        bank.mockEinfuegen(vonKontoMock);
+        bank.mockEinfuegen(nachKontoMock);
+//        bank.getKontenListe().put(vonKontonr, vonKontoMock);
+//        bank.getKontenListe().put(nachKontonr, nachKontoMock);
 
-        assertTrue(result);
-        verify((Ueberweisungsfaehig)kontoUeberweisungsfähig).ueberweisungAbsenden(500,"Empfaenger",2,12345,"Überweisungstest");
-        verify((Ueberweisungsfaehig)kontoUeberweisungsfähig2).ueberweisungEmpfangen(500,"Sender",1,12345,"Überweisungstest");
+        // Definieren des Verhaltens der Mock-Objekte
+        when(vonKontoMock.getKontonummer()).thenReturn(vonKontonr);
+        when(nachKontoMock.getKontonummer()).thenReturn(nachKontonr);
+        when(((Ueberweisungsfaehig) vonKontoMock).ueberweisungAbsenden(betrag, nachKontoMock.getInhaber().getName(), nachKontonr, bank.getBankleitzahl(), verwendungszweck)).thenReturn(true);
+
+        // Durchführen des Tests
+        boolean result = bank.geldUeberweisen(vonKontonr, nachKontonr, betrag, verwendungszweck);
+
+        // Überprüfen der Ergebnisse
+        assertTrue(result, "Überweisung sollte erfolgreich sein");
+        verify((Ueberweisungsfaehig) vonKontoMock).ueberweisungAbsenden(betrag, nachKontoMock.getInhaber().getName(), nachKontonr, bank.getBankleitzahl(), verwendungszweck);
+        verify((Ueberweisungsfaehig) nachKontoMock).ueberweisungEmpfangen(betrag, vonKontoMock.getInhaber().getName(), vonKontonr, bank.getBankleitzahl(), verwendungszweck);
     }
 
-//    @Test
-//    void geldUeberweisen_normal_false_Test() throws Exception {
-//        when(kontoUeberweisungsfähig.abheben(5000.0)).thenReturn(false);
-//
-//        boolean result = bank.geldUeberweisen(1, 2, 5000.0, "Überweisungstest");
-//
-//        assertFalse(result);
-//        verify(kontoUeberweisungsfähig).abheben(5000.0);
-//        verify(kontoNichtUeberweisungsfähig, never()).einzahlen(5000.0);
-//    }
-//
-//    @Test
-//    void geldUeberweisen_exception_Test() {
-//        // UngueltigeKontonummerException
-//        assertThrows(UngueltigeKontonummerException.class, () -> bank.geldUeberweisen(999, 1, 500.0, "Überweisungstest"));
-//
-//        // NichtUeberweisungsfaehigException
-//        assertThrows(NichtUeberweisungsfaehigException.class, () -> bank.geldUeberweisen(1, 3, 500.0, "Überweisungstest"));
-//
-//        // GesperrtException
-//        when(kontoUeberweisungsfähig.istGesperrt()).thenReturn(true);
-//        assertThrows(GesperrtException.class, () -> bank.geldUeberweisen(1, 2, 500.0, "Überweisungstest"));
-//    }
+    @Test
+    void testGeldUeberweisenFehlgeschlagen() throws UngueltigeKontonummerException, NichtUeberweisungsfaehigException, GesperrtException {
+        // Vorbereiten der Testdaten
+        long vonKontonr = 1, nachKontonr = 2;
+        double betrag = 100.0;
+        String verwendungszweck = "Testüberweisung";
+
+        // Hinzufügen der Mock-Konten zur Bank
+        bank.mockEinfuegen(vonKontoMock);
+        bank.mockEinfuegen(nachKontoMock);
+
+        // Definieren des Verhaltens der Mock-Objekte
+        when(vonKontoMock.getKontonummer()).thenReturn(vonKontonr);
+        when(nachKontoMock.getKontonummer()).thenReturn(nachKontonr);
+        when(((Ueberweisungsfaehig) vonKontoMock).ueberweisungAbsenden(betrag, nachKontoMock.getInhaber().getName(), nachKontonr, bank.getBankleitzahl(), verwendungszweck)).thenReturn(false);
+
+        // Durchführen des Tests
+        boolean result = bank.geldUeberweisen(vonKontonr, nachKontonr, betrag, verwendungszweck);
+
+        // Überprüfen der Ergebnisse
+        assertFalse(result, "Überweisung sollte fehlschlagen");
+        verify((Ueberweisungsfaehig) vonKontoMock).ueberweisungAbsenden(betrag, nachKontoMock.getInhaber().getName(), nachKontonr, bank.getBankleitzahl(), verwendungszweck);
+        verify((Ueberweisungsfaehig) nachKontoMock, never()).ueberweisungEmpfangen(betrag, vonKontoMock.getInhaber().getName(), vonKontonr, bank.getBankleitzahl(), verwendungszweck);
+    }
 }
