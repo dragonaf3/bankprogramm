bf6f185924b6250282c2c62ed14dff9d844535e7
drago <97@dragonaf.eu>
2024-01-15 04:07:03 +0100

    Übung 12 (vollständig, Kleiner Fehler verbessert)

--- src/main/java/bankprojekt/verwaltung/KontoBenachrichtigung.java
+++ src/main/java/bankprojekt/verwaltung/KontoBenachrichtigung.java
@@ -4,12 +4,17 @@ import bankprojekt.verarbeitung.Konto;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.Serial;
+import java.io.Serializable;
 
 /**
  * Die Klasse KontoBenachrichtigung implementiert das PropertyChangeListener-Interface.
  * Sie wird verwendet, um Änderungen am Kontostand zu überwachen und bei Änderungen eine Benachrichtigung auszugeben.
  */
-public class KontoBenachrichtigung implements PropertyChangeListener {
+public class KontoBenachrichtigung implements PropertyChangeListener, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     /**
      * Diese Methode wird aufgerufen, wenn eine Änderung an einer beobachteten Eigenschaft auftritt.
      * In diesem Fall wird eine Nachricht mit der Kontonummer und dem neuen Kontostand ausgegeben.
